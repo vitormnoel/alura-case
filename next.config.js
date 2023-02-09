d7e4702d53c1e306be7adeb2d20d36ed57b164ea
Vitor Manoel <65787376+vitormnoel@users.noreply.github.com>
2023-02-09 09:42:01 -0800

    packages deleted

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
     async redirects() {
         return [
             {
-                source: '/home',
-                destination: '/',
+                source: '/perguntas',
+                destination: '/faq/',
                 permanent: true,
             }
         ]
